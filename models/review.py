0bc4631501bd3a796a32d81c473f3a28d7630b19
chriswill88 <williechri79@gmail.com>
2019-12-21 05:34:05 +0000

    ABNB v2. Refactor place.py, review.py, user.py for db storage.

--- models/review.py
+++ models/review.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """This is the review class"""
 from models.base_model import BaseModel, Base
+from sqlalchemy import Column, Integer, String, ForeignKey
 
 
 class Review(BaseModel, Base):
@@ -10,7 +11,26 @@ class Review(BaseModel, Base):
         user_id: user id
         text: review description
     """
-    __tablename__ = 'reviews'
-    place_id = ""
-    user_id = ""
-    text = ""
+    # New
+    __tablename__ = "reviews"
+
+    text = Column(
+        String(1024),
+        nullable=False
+    )
+    place_id = Column(
+        String(60),
+        ForeignKey('places.id'),
+        nullable=False
+    )
+    user_id = Column(
+        String(60),
+        ForeignKey('userss.id'),
+        nullable=False
+    )
+
+    # -----------------------
+    # Old
+    # place_id = ""
+    # user_id = ""
+    # text = ""
--- models/user.py
+++ models/user.py
@@ -1,9 +1,20 @@
 #!/usr/bin/python3
 """This is the user class"""
-from models.base_model import BaseModel
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import relationship
+from models.base_model import BaseModel, Base
+"""
+Things that might need change:
+1. Base inheritance Configuration
+do i need to declare base or do
+import it from file storage?
+
+fix: in base_model i have to declare base
+import base from base_model!
+"""
 
 
-class User(BaseModel):
+class User(BaseModel, Base):
     """This is the class for user
     Attributes:
         email: email address
@@ -11,7 +22,28 @@ class User(BaseModel):
         first_name: first name
         last_name: last name
     """
-    email = ""
-    password = ""
-    first_name = ""
-    last_name = ""
+    # New
+    __tablename__ = 'users'
+
+    email = Column(
+        String(128),
+        nullable=False)
+    password = Column(
+        String(128),
+        nullable=False)
+    first_name = Column(
+        String(128),
+        nullable=True)
+    last_name = Column(
+        String(128),
+        nullable=True)
+
+    places = relationship("Place", backref='user')
+    reviews = relationship("Review", backref='user')
+
+    # -------------------------------
+    # Old
+    # email = ""
+    # password = ""
+    # first_name = ""
+    # last_name = ""
