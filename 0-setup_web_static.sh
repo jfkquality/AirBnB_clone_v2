bebdde6b641faae393ef8cc0d7044d4a553c2b47
jfkquality <jfkquality@gmail.com>
2020-01-17 04:45:40 +0000

    ABNB deploy static. Task 0. Fix html, sed.

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -4,15 +4,37 @@ sudo apt-get update -y
 sudo apt-get -y install nginx
 sudo mkdir -p /var/www/html
 sudo mkdir -p /data/web_static/{releases/test,shared}
-echo "Web-static deployed!" > /data/web_static/releases/test/index.html
+cat <<EOF > /data/web_static/releases/test/index.html
+<html>
+  <head>
+  </head>
+  <body>
+    Holberton School
+  </body>
+</html>
+EOF
 sudo ln -s /data/web_static/current /data/web_static/releases/test/
 sudo chown -hR ubuntu:ubuntu /data
-sudo sed -i '/http {/a
-    \tserver {
-    \t\tlocation /hbnb_static/ {
-      \t\t\talias /data/web_static/current
-    \t\t}
-    \t}
-' /etc/nginx/nginx.conf
+
+location=$(cat <<EOF
+      location /hbnb_static/ {
+        alias /data/web_static/current;
+      }
+EOF
+)
+
+sed -i.bak "/^.\{0\}server {/a ${location//$'\n'/$'\\\n'}" /etc/nginx/sites-available/default
+
+# sed -i.bak "/http {/a ${location//$'\n'/$'\\\n'}" /data/web_static/current
+
+# sudo sed -i '/http {/a\
+#     \tserver {\
+#     \t\tlocation /hbnb_static/ {\
+#       \t\t\talias /data/web_static/current\
+#     \t\t}\
+#     \t}\
+# ' /etc/nginx/nginx.conf
+
 # sed -i "/http {/a ${location//$'\n' /$'\\\n'}" /etc/nginx/nginx.conf
+
 sudo service nginx restart
