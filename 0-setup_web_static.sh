0cbc8c19aeacbfe97648d4e3a3054d7f93541568
jfkquality <jfkquality@gmail.com>
2020-01-17 05:10:23 +0000

    ABNB deploy static. Task 0. Add -f to ln command to delete if exists.

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -13,28 +13,13 @@ cat <<EOF > /data/web_static/releases/test/index.html
   </body>
 </html>
 EOF
-sudo ln -s /data/web_static/current /data/web_static/releases/test/
-sudo chown -hR ubuntu:ubuntu /data
-
+sudo ln -sf /data/web_static/current /data/web_static/releases/test/
+sudo chown -hR ubuntu:ubuntu /data/
 location=$(cat <<EOF
       location /hbnb_static/ {
         alias /data/web_static/current;
       }
 EOF
 )
-
 sed -i.bak "/^.\{0\}server {/a ${location//$'\n'/$'\\\n'}" /etc/nginx/sites-available/default
-
-# sed -i.bak "/http {/a ${location//$'\n'/$'\\\n'}" /data/web_static/current
-
-# sudo sed -i '/http {/a\
-#     \tserver {\
-#     \t\tlocation /hbnb_static/ {\
-#       \t\t\talias /data/web_static/current\
-#     \t\t}\
-#     \t}\
-# ' /etc/nginx/nginx.conf
-
-# sed -i "/http {/a ${location//$'\n' /$'\\\n'}" /etc/nginx/nginx.conf
-
 sudo service nginx restart
