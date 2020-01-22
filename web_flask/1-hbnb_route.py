b9126b2a427caf7c97bf75c5e68f2d7f83bf357b
jfkquality <jfkquality@gmail.com>
2020-01-22 19:40:46 +0000

    web flask. Tasks 1. Delete ! after HBNB.

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -11,7 +11,7 @@ def hello_hbnb():
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
-    return 'HBNB!'
+    return 'HBNB'
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=5000)
