a48852770030854099ffc11c1b4109f88f5d24aa
jfkquality <jfkquality@gmail.com>
2020-01-22 05:15:01 +0000

    web flask. Task 2. Add docstrings.

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -5,14 +5,17 @@ app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
+    """Serve Hello HBNB!"""
     return 'Hello HBNB!'
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
+    """Serve HBNB."""
     return 'HBNB!'
 
 @app.route('/c/<string:text>', strict_slashes=False)
 def cisfun(text):
+    """Serve C + text; replace _ with space."""
     return 'C %s' % text.replace("_", " ")
 
 if __name__ == "__main__":
